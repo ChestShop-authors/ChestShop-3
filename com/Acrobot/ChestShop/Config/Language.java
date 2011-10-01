cae56bca2dae5136e1d53b0a3d3f3c8d679de8a6
Ryan van Zeben <vanZeben@live.ca>
2011-10-01 04:11:59 -0400

    Support for Shop Item Blacklist

--- com/Acrobot/ChestShop/Config/Language.java
+++ com/Acrobot/ChestShop/Config/Language.java
@@ -38,7 +38,8 @@ public enum Language {
 
     NO_PERMISSION("You don't have permissions to do that!"),
     INCORRECT_ITEM_ID("You have specified invalid item id!"),
-
+    BLACKLISTED_ITEM("You can't have &item in your shop!"),
+    
     TOWNY_CANNOT_CREATE_SHOP_HERE("You can't create a shop here!");
 
     private final String text;
