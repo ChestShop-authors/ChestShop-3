80f658e748e82d7fba379e59a017f4aa8cb71d08
meiamsome <meiamsome@hotmail.com>
2012-08-18 21:56:46 +0200

    Added roundDown
    Added another round function as required.

--- com/Acrobot/Breeze/Utils/NumberUtil.java
+++ com/Acrobot/Breeze/Utils/NumberUtil.java
@@ -65,13 +65,22 @@ public class NumberUtil {
     }
 
     /**
-     * Rounds the number up to two digit points (Can be inaccurate due to using decimal-points)
+     * Rounds the number up to two decimal points (Can be inaccurate due to using decimal-points)
      *
      * @param number Number to round
      * @return Rounded number
      */
     public static double roundUp(double number) {
         return Math.ceil(number * 100) / 100;
+    }
+    /**
+     * Rounds the number down to two decimal points
+     * 
+     * @param number Number to round
+     * @return Rounded number
+     */
+    public static double roundDown(double number) {
+        return Math.floor(number * 100) / 100;
     }
 
     /**
