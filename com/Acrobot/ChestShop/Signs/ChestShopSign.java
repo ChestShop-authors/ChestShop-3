bbeec231eccf3c90d7a143d23f07b0178a63a7d4
Acrobot <andrzejp@dysleksja.pl>
2012-09-16 11:53:38 +0200

    Added '-' to the supported name character list

--- com/Acrobot/ChestShop/Signs/ChestShopSign.java
+++ com/Acrobot/ChestShop/Signs/ChestShopSign.java
@@ -23,10 +23,10 @@ public class ChestShopSign {
     public static final byte ITEM_LINE = 3;
 
     public static final Pattern[] SHOP_SIGN_PATTERN = {
-            Pattern.compile("^[\\w ]*$"),
-            Pattern.compile("[0-9]+"),
+            Pattern.compile("^[\\w -]*$"),
+            Pattern.compile("^[0-9]+$"),
             Pattern.compile("(?i)^[\\d.bs(free) :]+$"),
-            Pattern.compile("[\\w : -]+")
+            Pattern.compile("^[\\w : -]+$")
     };
 
     public static boolean isAdminShop(Inventory ownerInventory) {
