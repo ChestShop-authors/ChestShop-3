22c5e20703002c24621a4c85e8e5da7fae337766
Acrobot <andrzejp@dysleksja.pl>
2012-11-23 21:03:02 +0100

    Changed how uName works
    (I still don't like the uName class at all, it will probably change in
    the near future)

--- com/Acrobot/ChestShop/Utils/uName.java
+++ com/Acrobot/ChestShop/Utils/uName.java
@@ -1,14 +1,18 @@
 package com.Acrobot.ChestShop.Utils;
 
-import com.Acrobot.ChestShop.Config.BreezeConfiguration;
 import com.Acrobot.ChestShop.Permission;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Acrobot
  */
 public class uName {
-    public static BreezeConfiguration config;
+    public static YamlConfiguration config;
+    public static File file;
 
     public static String getName(String shortName) {
         return config.getString(shortName, shortName);
@@ -20,7 +24,7 @@ public class uName {
         }
 
         config.set(stripName(name), name);
-        config.reload();
+        reload();
     }
 
     public static String stripName(String name) {
@@ -33,5 +37,18 @@ public class uName {
 
     public static boolean canUseName(Player player, String name) {
         return shortenName(player).equals(name) || Permission.otherName(player, name);
+    }
+
+    public static void load() {
+        config = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public static void reload() {
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        load();
     }
 }
