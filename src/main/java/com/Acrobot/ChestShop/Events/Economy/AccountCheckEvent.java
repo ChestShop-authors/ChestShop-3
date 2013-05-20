e11bed92a3980f6015d98cb354e33ef66717822d
Acrobot <andrzejp@dysleksja.pl>
2013-05-20 19:09:45 +0200

    Add AccountCheckEvent

---
+++ src/main/java/com/Acrobot/ChestShop/Events/Economy/AccountCheckEvent.java
@@ -0,0 +1,48 @@
+package com.Acrobot.ChestShop.Events.Economy;
+
+import org.bukkit.World;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Checks for the existance of an account
+ *
+ * @author Acrobot
+ */
+public class AccountCheckEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+
+    private String account;
+    private World world;
+
+    public AccountCheckEvent(String account, World world) {
+        this.account = account;
+        this.world = world;
+    }
+
+    public AccountCheckEvent(String account) {
+        this.account = account;
+    }
+
+    /**
+     * @return Account which is being checked
+     */
+    public String getAccount() {
+        return account;
+    }
+
+    /**
+     * @return The world in which the check occurs
+     */
+    public World getWorld() {
+        return world;
+    }
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
