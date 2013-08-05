42ca03b780b7b949e869edef9114c786e4eea3cf
Acrobot <andrzejp@dysleksja.pl>
2013-08-05 02:06:01 +0200

    Send the transaction message in an async thread

--- src/main/java/com/Acrobot/ChestShop/Listeners/PostTransaction/TransactionLogger.java
+++ src/main/java/com/Acrobot/ChestShop/Listeners/PostTransaction/TransactionLogger.java
@@ -7,6 +7,7 @@ import com.Acrobot.ChestShop.DB.Transaction;
 import com.Acrobot.ChestShop.Events.TransactionEvent;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
@@ -20,24 +21,28 @@ public class TransactionLogger implements Listener {
     private static final String BUY_MESSAGE = "%1$s bought %2$s for %3$.2f from %4$s at %5$s";
     private static final String SELL_MESSAGE = "%1$s sold %2$s for %3$.2f to %4$s at %5$s";
 
-    @EventHandler
-    public static void onTransaction(TransactionEvent event) {
-        String template = (event.getTransactionType() == BUY ? BUY_MESSAGE : SELL_MESSAGE);
+    @EventHandler(priority = EventPriority.MONITOR)
+    public static void onTransaction(final TransactionEvent event) {
+        ChestShop.getBukkitServer().getScheduler().runTaskAsynchronously(ChestShop.getPlugin(), new Runnable() {
+            @Override public void run() {
+                String template = (event.getTransactionType() == BUY ? BUY_MESSAGE : SELL_MESSAGE);
 
-        StringBuilder items = new StringBuilder(50);
+                StringBuilder items = new StringBuilder(50);
 
-        for (ItemStack item : event.getStock()) {
-            items.append(item.getAmount()).append(' ').append(getSignName(item));
-        }
+                for (ItemStack item : event.getStock()) {
+                    items.append(item.getAmount()).append(' ').append(getSignName(item));
+                }
 
-        String message = String.format(template,
-                event.getClient().getName(),
-                items.toString(),
-                event.getPrice(),
-                event.getOwner().getName(),
-                locationToString(event.getSign().getLocation()));
+                String message = String.format(template,
+                        event.getClient().getName(),
+                        items.toString(),
+                        event.getPrice(),
+                        event.getOwner().getName(),
+                        locationToString(event.getSign().getLocation()));
 
-        ChestShop.getBukkitLogger().info(message);
+                ChestShop.getBukkitLogger().info(message);
+            }
+        });
     }
 
     @EventHandler
