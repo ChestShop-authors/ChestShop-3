4194a09c866492a055f9169ab51dd94bb25ac722
Faris Rehman <farisrehman@hotmail.co.uk>
2014-05-20 22:18:51 +0100

    Fixed missing code

--- src/main/java/com/Acrobot/ChestShop/Listeners/Player/PlayerLeave.java
+++ src/main/java/com/Acrobot/ChestShop/Listeners/Player/PlayerLeave.java
@@ -1,7 +1,19 @@
 package com.Acrobot.ChestShop.Listeners.Player;
 
+import com.Acrobot.ChestShop.Commands.Toggle;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
 /**
  * @author KingFaris10
  */
-public class PlayerLeave {
+public class PlayerLeave implements Listener {
+
+    @EventHandler
+    public static void onPlayerLeave(PlayerQuitEvent event) {
+        if (Toggle.isIgnoring(event.getPlayer())) Toggle.setIgnoring(event.getPlayer(), false);
+    }
+
 }
