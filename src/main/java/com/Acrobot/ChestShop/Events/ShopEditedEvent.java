fac9a18b8f5202363eb833bd5805f9434db1221c
Andrzej Pomirski <andrzej.pomirski@gmail.com>
2015-03-17 11:36:55 +0100

    Add a simple shop edited event, not yet working

---
+++ src/main/java/com/Acrobot/ChestShop/Events/ShopEditedEvent.java
@@ -0,0 +1,55 @@
+package com.Acrobot.ChestShop.Events;
+
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Represents an event caused by a shop modification
+ * @deprecated - not yet implemented
+ * TODO: Implement
+ * @author Andrzej Pomirski
+ */
+public class ShopEditedEvent extends Event  {
+    private static final HandlerList handlers = new HandlerList();
+
+    private final Player modifier;
+    private final Sign sign;
+    private final String[] signLines;
+
+    public ShopEditedEvent(Player modifier, Sign sign, String[] signLines) {
+        this.modifier = modifier;
+        this.sign = sign;
+        this.signLines = signLines;
+    }
+
+    /**
+     * @return The person who initiated the event
+     */
+    public Player getModifier() {
+        return modifier;
+    }
+
+    /**
+     * @return Shop sign
+     */
+    public Sign getSign() {
+        return sign;
+    }
+
+    /**
+     * @return Shop sign text
+     */
+    public String[] getSignLines() {
+        return signLines;
+    }
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
