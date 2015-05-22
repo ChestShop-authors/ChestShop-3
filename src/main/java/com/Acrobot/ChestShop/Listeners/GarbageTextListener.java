6982ef38d149b04888e8517762007e2fd10780dd
Andrzej Pomirski <andrzej.pomirski@gmail.com>
2015-05-22 13:26:42 +0200

    Should fix the Mac issues

---
+++ src/main/java/com/Acrobot/ChestShop/Listeners/GarbageTextListener.java
@@ -0,0 +1,30 @@
+package com.Acrobot.ChestShop.Listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
+
+/**
+ * Removes garbage characters from the sign
+ *
+ * @author Andrzej Pomirski
+ */
+public class GarbageTextListener implements Listener {
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public static void filterGarbage(SignChangeEvent event) {
+        for (int i = 0; i < 4; ++i) {
+            String line = event.getLine(i);
+            StringBuilder output = new StringBuilder(line.length());
+
+            for (char character : line.toCharArray()) {
+                if (character < 0xF700 || character > 0xF747) {
+                    output.append(character);
+                }
+            }
+
+            event.setLine(i, output.toString());
+        }
+    }
+}
