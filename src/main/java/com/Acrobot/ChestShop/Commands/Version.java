b9f9166d6c80234f2a9e50588a7882ba8af20713
Phoenix616 <mail@moep.tv>
2017-07-04 15:18:00 +0100

    Add permission check to version and reload command

--- src/main/java/com/Acrobot/ChestShop/Commands/Version.java
+++ src/main/java/com/Acrobot/ChestShop/Commands/Version.java
@@ -4,6 +4,7 @@ import com.Acrobot.Breeze.Configuration.Configuration;
 import com.Acrobot.ChestShop.ChestShop;
 import com.Acrobot.ChestShop.Configuration.Messages;
 import com.Acrobot.ChestShop.Configuration.Properties;
+import com.Acrobot.ChestShop.Permission;
 import com.Acrobot.ChestShop.UUIDs.NameManager;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -15,6 +16,11 @@ import org.bukkit.command.CommandSender;
  */
 public class Version implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!Permission.has(sender, Permission.ADMIN)) {
+            sender.sendMessage(Messages.prefix(Messages.ACCESS_DENIED));
+            return true;
+        }
+
         if (args.length > 0 && args[0].equals("reload")) {
             Configuration.pairFileAndClass(ChestShop.loadFile("config.yml"), Properties.class);
             Configuration.pairFileAndClass(ChestShop.loadFile("local.yml"), Messages.class);
