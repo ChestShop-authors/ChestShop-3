39d1c13491b7cbf9a979c64acad01af745685865
Phoenix616 <mail@moep.tv>
2020-02-22 17:45:59 +0100

    Block change block event for shop containers/signs

--- src/main/java/com/Acrobot/ChestShop/Listeners/Block/Break/SignBreak.java
+++ src/main/java/com/Acrobot/ChestShop/Listeners/Block/Break/SignBreak.java
@@ -18,6 +18,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.*;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.material.Directional;
 import org.bukkit.material.PistonBaseMaterial;
@@ -115,6 +116,13 @@ public class SignBreak implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public static void onIgnite(BlockBurnEvent event) {
+        if (!canBlockBeBroken(event.getBlock(), null)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public static void onEntityChangeBlock(EntityChangeBlockEvent event) {
         if (!canBlockBeBroken(event.getBlock(), null)) {
             event.setCancelled(true);
         }
