e22a50e1cf466e970da93d58be269663a02ae216
Phoenix616 <mail@moep.tv>
2020-05-16 22:33:40 +0100

    Only close our own inventories on teleport (Fixes #312)

--- src/main/java/com/Acrobot/ChestShop/Listeners/Player/PlayerTeleport.java
+++ src/main/java/com/Acrobot/ChestShop/Listeners/Player/PlayerTeleport.java
@@ -1,9 +1,13 @@
 package com.Acrobot.ChestShop.Listeners.Player;
 
+import com.Acrobot.ChestShop.Signs.ChestShopSign;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.DoubleChest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.InventoryHolder;
 
 /**
  * A fix for a CraftBukkit bug.
@@ -14,11 +18,19 @@ public class PlayerTeleport implements Listener {
 
     @EventHandler
     public static void onPlayerTeleport(PlayerTeleportEvent event) {
-        if (event.getPlayer() == null || event.getPlayer().getOpenInventory() == null) {
+        InventoryHolder holder = event.getPlayer().getOpenInventory().getTopInventory().getHolder();
+        if (!(holder instanceof BlockState)) {
             return;
         }
 
-        if (event.getPlayer().getOpenInventory().getType() != InventoryType.CRAFTING) {
+        Block container;
+        if (holder instanceof DoubleChest) {
+            container = ((DoubleChest) holder).getLocation().getBlock();
+        } else {
+            container = ((BlockState) holder).getBlock();
+        }
+
+        if (ChestShopSign.isShopBlock(container)) {
             event.getPlayer().closeInventory();
         }
     }
