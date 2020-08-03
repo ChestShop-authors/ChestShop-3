8f0a313403f1f411830504ff9215f96af346c484
LogGits <landogowner@gmail.com>
2020-08-03 17:49:27 +1000

    Make ItemInfoEvent cancellable.

--- src/main/java/com/Acrobot/ChestShop/Events/ItemInfoEvent.java
+++ src/main/java/com/Acrobot/ChestShop/Events/ItemInfoEvent.java
@@ -1,6 +1,7 @@
 package com.Acrobot.ChestShop.Events;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
@@ -10,15 +11,17 @@ import org.bukkit.inventory.ItemStack;
  *
  * @author Acrobot
  */
-public class ItemInfoEvent extends Event {
+public class ItemInfoEvent extends Event implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
 
     private CommandSender sender;
     private ItemStack item;
+    private boolean cancelled;
 
     public ItemInfoEvent(CommandSender sender, ItemStack item) {
         this.sender = sender;
         this.item = item;
+        this.cancelled = false;
     }
 
     /**
@@ -41,5 +44,15 @@ public class ItemInfoEvent extends Event {
 
     public static HandlerList getHandlerList() {
         return handlers;
+    }
+    
+    @Override
+    public boolean isCancelled () {
+        return cancelled;
+    }
+    
+    @Override
+    public void setCancelled (boolean b) {
+        cancelled = b;
     }
 }
