7aea9d852ccadade812dcdae2ad6ab3ece855115
Daniel Buchmann <daniel.buchmann@unit.no>
2021-02-19 22:01:50 +0100

    Make InvalidNameIgnorer reload friendly

--- src/main/java/com/Acrobot/ChestShop/Listeners/PreTransaction/InvalidNameIgnorer.java
+++ src/main/java/com/Acrobot/ChestShop/Listeners/PreTransaction/InvalidNameIgnorer.java
@@ -11,14 +11,13 @@ import java.util.regex.Pattern;
 
 public class InvalidNameIgnorer implements Listener {
 
-    private final static Pattern USERNAME_PATTERN = Pattern.compile(Properties.VALID_PLAYERNAME_REGEXP);
-
     @EventHandler(priority = EventPriority.LOWEST)
     public static void onPreTransaction(PreTransactionEvent event) {
         if (event.isCancelled()) {
             return;
         }
 
+        Pattern USERNAME_PATTERN = Pattern.compile(Properties.VALID_PLAYERNAME_REGEXP);
         String name = event.getClient().getName();
         if (ChestShopSign.isAdminShop(name) || !USERNAME_PATTERN.matcher(name).matches()) {
             event.setCancelled(PreTransactionEvent.TransactionOutcome.INVALID_CLIENT_NAME);
