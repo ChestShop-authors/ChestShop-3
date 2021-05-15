d3ed650b744a70a50a02d33f0f90f478ecdae0cf
Phoenix616 <max@themoep.de>
2021-05-16 00:15:59 +0100

    Make LWC donation and display protections work with shops

--- src/main/java/com/Acrobot/ChestShop/Plugins/LightweightChestProtection.java
+++ src/main/java/com/Acrobot/ChestShop/Plugins/LightweightChestProtection.java
@@ -120,7 +120,7 @@ public class LightweightChestProtection implements Listener {
             return;
         }
 
-        if (!lwc.canAccessProtection(player, protection) || protection.getType() == Protection.Type.DONATION) {
+        if (!lwc.canAccessProtection(player, protection) && !protection.getType().name().equals("DONATION") && !protection.getType().name().equals("DISPLAY")) {
             event.setResult(Event.Result.DENY);
         } else if (Properties.TURN_OFF_DEFAULT_PROTECTION_WHEN_PROTECTED_EXTERNALLY) {
             event.setResult(Event.Result.ALLOW);
