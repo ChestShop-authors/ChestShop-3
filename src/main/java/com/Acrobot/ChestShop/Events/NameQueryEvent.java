99c7cfb7de9cbbb61b6a422281e8005128e16675
Sulky_Whale <171066059+SulkyWhale@users.noreply.github.com>
2025-06-08 12:04:39 +0200

    Add javadoc

--- src/main/java/com/Acrobot/ChestShop/Events/NameQueryEvent.java
+++ src/main/java/com/Acrobot/ChestShop/Events/NameQueryEvent.java
@@ -3,6 +3,9 @@ package com.Acrobot.ChestShop.Events;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+/**
+ * Represents a request whether a name is allowed.
+ */
 public class NameQueryEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
     private final String name;
@@ -12,14 +15,29 @@ public class NameQueryEvent extends Event {
         this.name = name;
     }
 
+    /**
+     * The name to check for.
+     *
+     * @return The name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Whether or not the name is valid.
+     *
+     * @return Whether the name is valid
+     */
     public boolean isValid() {
         return valid;
     }
 
+    /**
+     * Set whether the name is valid.
+     *
+     * @param valid Whether the name is valid
+     */
     public void setValid(boolean valid) {
         this.valid = valid;
     }
